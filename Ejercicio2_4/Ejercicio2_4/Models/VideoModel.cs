a4304bbb95f9118560ef148fc29e65808bf8065a
Joel Reyes <joel.rys98@gmail.com>
2023-03-27 23:34:18 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2_4/Ejercicio2_4/Models/VideoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Ejercicio2_4.Models
+{
+    public class VideoModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Uri { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
